5106c26a3b16b902fb2c4fbe5b3e44e938139661
Sergey Kozlov <kozlov@megaputer.ru>
2018-07-27 21:07:17 +0300

    some imprvements

--- src/table.ts
+++ src/table.ts
@@ -1,6 +1,6 @@
 import {
   Table as TableBase,
-  TableArgs,
+  ExecuteArgs,
   Cells,
   Columns,
   ColumnAttr,
@@ -14,7 +14,8 @@ import {
   ValueCond
 } from 'objio-object/table';
 import { Database } from 'sqlite3';
-import { SERIALIZER, EXTEND, CSVReader, CSVBunch } from 'objio';
+import { SERIALIZER, EXTEND } from 'objio';
+import { CSVReader, CSVBunch } from 'objio/server';
 import { FileObject } from 'objio-object/server/file-object';
 
 let db: Database;
@@ -125,6 +126,10 @@ function createTable(db: Database, table: string, columns: Columns): Promise<any
   return exec(db, `create table ${table} (${sql})`);
 }
 
+function deleteTable(db: Database, table: string): Promise<Database> {
+  return exec(db, `drop table if exists ${table}`).then(() => db);
+}
+
 function loadTableInfo(db: Database, table: string): Promise<Columns> {
   return all<ColumnAttr>(db, `pragma table_info(${table})`).then(res => {
     return res.map(row => ({name: row['name'], type: row['type']}));
@@ -153,6 +158,32 @@ function insert(db: Database, table: string, values: {[col: string]: Array<strin
   return run(db, sql, valuesArr);
 }
 
+function createIdColumn(cols: Array<ColumnAttr>, idColName?: string): ColumnAttr {
+  let idCol: ColumnAttr;
+  if (!idColName) { // create column
+    idColName = idColName || 'row_uid';
+    while (cols.find(col => col.name == idColName)) {
+      idColName = 'row_uid_' + Math.round(Math.random() * 100000).toString(16);
+    }
+  } else {  //  find
+    idCol = cols.find(col => col.name == idColName);
+  }
+
+  if (!idCol) {
+    idCol = {
+      name: idColName,
+      type: 'INTEGER',
+      autoInc: true,
+      notNull: true,
+      primary: true,
+      unique: true
+    };
+    cols.splice(0, 0, idCol);
+  }
+
+  return idCol;
+}
+
 export class Table extends TableBase {
   constructor() {
     super();
@@ -162,11 +193,15 @@ export class Table extends TableBase {
       pushCells: (args: PushRowArgs) => this.pushCells(args),
       updateCells: (args: UpdateRowArgs) => this.updateCells(args),
       removeRows: (args: RemoveRowsArgs) => this.removeRows(args),
-      execute: (args: TableArgs) => this.execute(args)
+      execute: (args: ExecuteArgs) => this.execute(args)
     });
 
     this.holder.addEventHandler({
       onLoad: () => {
+        // not configured
+        if (!this.table)
+          return;
+
         return (
           this.openDB()
           .then(db => {
@@ -210,8 +245,9 @@ export class Table extends TableBase {
         });
       });
 
-      return this.pushCells({values}).then(() => {
-        this.state.setProgress(bunch.progress).save();
+      return this.pushCells({values, updRowCounter: false}).then(() => {
+        this.state.setProgress(bunch.progress);
+        this.totalRowsNum += rows.length;
       });
     };
 
@@ -220,56 +256,59 @@ export class Table extends TableBase {
     );
   }
 
-  execute(args: TableArgs): Promise<any> {
-    this.table = args.table;
-    this.columns = (args.columns || []).map(column => ({...column}));
-
-    let idCol: ColumnAttr;
-    if (!args.idColumn) {
-      while (idCol = this.findColumn(this.idColumn)) {
-        this.idColumn = 'row_uid_' + Math.round(Math.random() * 100000).toString(16);
-      }
-    } else {
-      this.idColumn = args.idColumn;
-      idCol = this.findColumn(args.idColumn);
-    }
-
-    idCol = {
-      name: !idCol ? this.idColumn : idCol.name,
-      type: 'INTEGER',
-      autoInc: true,
-      notNull: true,
-      primary: true,
-      unique: true
-    };
-
+  execute(args: ExecuteArgs): Promise<any> {
+    let startTime: number;
     let columns: Columns = [];
-    let task: Promise<any>;
-    if (args.srcId) {
-      task = this.holder.getObject<FileObject>(args.srcId)
-      .then(csv => this.readColumns(csv))
-      .then(cols => {
-        columns = cols;
-        this.columns = [idCol, ...cols];
-      });
+    let readRowCols: Columns = [];
+    let idCol: ColumnAttr;
+
+    let prepCols: Promise<Array<ColumnAttr>>;
+    if (args.fileObjId) {
+      prepCols = this.holder.getObject<FileObject>(args.fileObjId)
+      .then(csv => this.readColumns(csv));
     } else {
-      this.columns.splice(0, 0, idCol);
-      task = Promise.resolve();
+      prepCols = Promise.resolve( (args.columns || []).map(col => ({...col})) );
     }
 
-    return task.then(() => this.openDB())
-    .then(db => createTable(db, this.table, this.columns))
+    const task  = prepCols.then(cols => {
+      // append idColumn if need
+      readRowCols = cols.slice();
+      idCol = createIdColumn(cols, args.idColumn);
+      columns = cols;
+      return this.openDB();
+    })
+    .then(db => deleteTable(db, args.table))
+    .then(db => createTable(db, args.table, columns))
     .then(() => {
-      this.state.setStateType('inProgress').save();
+      this.totalRowsNum = 0;
+      this.columns = columns;
+      this.table = args.table;
+      this.idColumn = idCol.name;
       this.holder.save();
+    });
 
-      if (args.srcId) {
-        this.holder.getObject<FileObject>(args.srcId)
-        .then((obj) => this.readRows(obj, columns, 1, 50))
-        .then(() => {
-          this.state.setProgress(1).setStateType('valid').save();
-        });
-      }
+    if (!args.fileObjId)
+      return task;
+
+    return task.then(() => {
+      this.holder.getObject<FileObject>(args.fileObjId)
+      .then(obj => {
+        this.state.setStateType('in progress').save();
+        this.holder.save();
+        startTime = Date.now();
+        return this.readRows(obj, readRowCols, 1, 50);
+      })
+      .then(() => this.openDB())
+      .then(db => this.updateRowNum(db))
+      .then(() => {
+        this.fileObjId = args.fileObjId;
+        this.lastExecuteTime = Date.now() - startTime;
+        this.holder.save();
+        this.state.setProgress(1);
+        this.state.setStateType('valid').save();
+      }).catch(err => {
+        this.state.addError(err.toString()).save();
+      });
     });
   }
 
@@ -286,6 +325,9 @@ export class Table extends TableBase {
   }
 
   pushCells(args: PushRowArgs): Promise<number> {
+    if (!this.table)
+      throw 'table not defined';
+
     const values = {...args.values};
     delete values[this.getIdColumn()];
 
@@ -294,7 +336,7 @@ export class Table extends TableBase {
       this.openDB()
       .then(dbobj => db = dbobj)
       .then(() => insert(db, this.table, values))
-      .then(() => this.updateRowNum(db))
+      .then(() => args.updRowCounter != false && this.updateRowNum(db))
     );
   }
 
