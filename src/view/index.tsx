09010bd6a7a78d1cc8a7a2cbca4d3c59b48d8ee0
xega <xxegax@gmail.com>
2018-12-21 10:06:06 +0300

    images

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -2,11 +2,13 @@ import * as React from 'react';
 import { OBJIOItemClassViewable, registerViews } from 'objio-object/view/config';
 import { Database, DatabaseView, Props } from './database-view';
 import { Icon } from 'ts-react-ui/icon';
+import 'ts-react-ui/typings';
+import * as sqliteIcon from '../images/sqlite-icon.png';
 
 export function getViews(): Array<OBJIOItemClassViewable> {
   registerViews({
     classObj: Database,
-    icons: { item: <Icon src='sqlite-db.png'/> },
+    icons: { item: <Icon src={sqliteIcon}/> },
     views: [{
       view: (props: Props) => <DatabaseView {...props}/>
     }],
