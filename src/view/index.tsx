9612a4f1141499300137057505fcc0d80d10c570
xega <xxegax@gmail.com>
2019-05-10 22:16:07 +0300

    icon

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,35 +1,23 @@
 import * as React from 'react';
-import { OBJIOItemClassViewable, registerViews } from 'objio-object/view/config';
 import { DatabaseHolder } from 'objio-object/client/database/database-holder';
 import { Database2 } from '../client/database2';
-import { Database, DatabaseView, Props } from './database-view';
 import { Icon } from 'ts-react-ui/icon';
 import 'ts-react-ui/typings';
-import * as sqliteIcon from '../images/sqlite-icon.png';
+import * as SQLITEIcon from '../images/sqlite.svg';
 import { ObjectToCreate }  from 'objio-object/common/interfaces';
+import { OBJIOItemClassViewable, registerViews } from 'objio-object/view/config';
 
 export function getObjectsToCreate(): Array<ObjectToCreate> {
   return [
     {
       name: 'sqlite3',
       desc: 'sqlite3 database',
+      icon: <Icon src={SQLITEIcon}/>,
       create: () => new DatabaseHolder({ impl: new Database2() })
     }
   ];
 }
 
 export function getViews(): Array<OBJIOItemClassViewable> {
-  registerViews({
-    classObj: Database,
-    icons: { item: <Icon src={sqliteIcon}/> },
-    views: [{
-      view: (props: Props) => <DatabaseView {...props}/>
-    }],
-    flags: [ 'create-wizard' ],
-    desc: 'SQLITE3 Database'
-  });
-
-  return [
-    Database
-  ];
+  return [];
 }
